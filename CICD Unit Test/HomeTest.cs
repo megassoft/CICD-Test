c8c8be0ffd6ef17a940f64609d89f30adba89227
megas <megassoft@users.noreply.github.com>
2022-02-26 20:09:54 -0500

    Update HomeTest.cs

--- CICD Unit Test/HomeTest.cs
+++ CICD Unit Test/HomeTest.cs
@@ -21,7 +21,7 @@ namespace CICD_Unit_Test
         [TestMethod]
         public void TestAdd()
         {
-            //fix from master 2
+            //fix from master 3
             const int EXPECTED = 5;
 
             int result = testHome.Add(X, Y);
