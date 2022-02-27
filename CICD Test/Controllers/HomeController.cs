59925cf65f268fba2852d150677c584a12b37304
Kaya <megassoft@hotmail.com>
2022-02-27 10:34:54 -0500

    up

--- CICD Test/Controllers/HomeController.cs
+++ CICD Test/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace CICD_Test.Controllers
           
         public int Add(int x, int y)
         {
-            //debug3 - done
+            //debug3 - done2
             int total = x + y;
 
 
